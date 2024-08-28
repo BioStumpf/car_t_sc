c72e3f53df895aee05f88c4c8b22ef46913e572a
BioStumpf <david-dsf@gmx.de>
2024-08-28 10:53:43 +0200

    Cellranger Jobs

---
+++ 02_code/cellranger_jobs/slurm_scripts/02_run_cellranger_P1_CSP_GEX.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=cellranger_P1_CSP_GEX
+#SBATCH --error=car_t_sc/01_data/raw/cellranger_CSP_GEX/err.txt
+#SBATCH --output=car_t_sc/01_data/raw/cellranger_CSP_GEX/output.out
+#SBATCH --mail-type=ALL #if you want to receive emails when your job starts/fails/finishes, very handy
+#SBATCH --mail-user=david.stumpf@stud-mail.uni-wuerzburg.de
+
+##change working directory
+cd /home/s377963/car_t_sc/01_data/raw/cellranger_CSP_GEX || exit
+
+## Run the Cell Ranger command
+/home/s377963/cellranger/cellranger-8.0.1/bin/cellranger count \
+  --id p1 \
+  --transcriptome /home/s377963/cellranger/refgenomes/refdata-gex-GRCm39-2024-A/ \
+  --create-bam false \
+  --feature-ref /home/s377963/car_t_sc/01_data/raw/cellranger/2024-06-07_24054SC_Luu_P1_cellranger/count/feature_reference.csv \
+  --libraries /home/s377963/car_t_sc/02_code/cellranger_jobs/library_CSVs/GEX_CSP/P1.csv \
+  --chemistry SC5P-R2
