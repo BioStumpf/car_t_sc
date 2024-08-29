d38f264cdafa53f93649f414b93070bda2ab9372
BioStumpf <david-dsf@gmx.de>
2024-08-29 08:56:22 +0200

    Add packages to funcs

--- 02_code/functions/functions_dat_processing.py
+++ 02_code/functions/functions_dat_processing.py
@@ -1,3 +1,6 @@
+import numpy as np
+from scipy.stats import median_abs_deviation
+
 #this function is to determine outliers. It can only be used after using sc.pp.calculate_qc_metrics().
 #it takes a specific qc metric, like total count or total gene count and extracts the column corresponding to this metric form the adata object.
 #then it computes wether the MAD (median absolute deviation) deviates nmads (e.g. 5) from the median. The output is an array of F/T.
