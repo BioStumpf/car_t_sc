e98603eedad209a31a2e028b4ca9f753ae85e1db
BioStumpf <david-dsf@gmx.de>
2024-09-05 10:35:58 +0200

    renamimg in hashing_columns func

--- 02_code/functions/functions_dat_processing.py
+++ 02_code/functions/functions_dat_processing.py
@@ -24,12 +24,23 @@ def is_qntl_outlier(adata, metric: str, qntl_threshhold: float):
 #This function takes the HTO feature types (assigned as Antibody Capture) and subsets the adata object to contain only this specific feature type.
 #This is saved into a pandas dataframe, which can finally be joined with the .obs dataframe.
 #Since no longer needed, the feature type is eliminated from the adtada.X matrix and only remains in the .obs df.
-def hashing_columns(data):   
+def hashing_columns(data, rename_to = None, rm_var_cols=False):   
+    #copy the andata object
     dat_cpy = data.copy() 
+    #find those andata.var rows matching antibody capture (which only works if you do not combine HTO with real antibody capture experiments)
     htos = dat_cpy.var['feature_types']  == 'Antibody Capture'
+    #create a dataframe containing the counts for all HTOs
     htos_df = dat_cpy[:, htos].copy().to_df()
+    #rename the dataframes columnames to whatever you find suitable (if you want to rename)
+    if rename_to:
+        #check if len of the list of names matches the len of the dataframe colnames
+        if len(rename_to) == len(htos_df.columns):
+            #if so, replace the old names with the new names
+            htos_df.columns = rename_to
+    #join the obs columns with the HTO columns
     dat_cpy.obs = dat_cpy.obs.join(htos_df)
-    dat_cpy = dat_cpy[:, ~htos].copy()
+    #if wanting to remove the var columns completely, remove them
+    if rm_var_cols: dat_cpy = dat_cpy[:, ~htos].copy()
     return dat_cpy
 
 #function for finding empty droplets in the raw data
