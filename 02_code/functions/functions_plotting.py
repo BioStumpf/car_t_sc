ccd4ed3455d76903ecbbf2e2f1746698eafcb4da
BioStumpf <david-dsf@gmx.de>
2024-12-13 16:31:35 +0100

    added merging function to CAR

--- 02_code/functions/functions_plotting.py
+++ 02_code/functions/functions_plotting.py
@@ -6,6 +6,8 @@ from matplotlib import pyplot as plt
 import anndata as ad
 import numpy as np
 import pandas as pd
+import os
+import regex as re
 
 #This function extracts a specific day and condition from the adata objecs classification
 def extract_DayCond(days, conditions, classification):
@@ -78,3 +80,43 @@ def stacked_barplot(adata, cell_subtype, obs_column):
     fig.suptitle(f'Fraction of {cell_subtype} T cells by Condition')
     # plt.tight_layout(rect=[0, 0.03, 1, 0.95])  # Adjust layout to fit suptitle
     plt.show()
+
+#this is to read the cellbarcodes that matched to the cart receptor
+def read_and_merge_CAR_annotation(Folder):
+    files = sorted(os.listdir(Folder))
+    VDJ_GEX_list = []
+    for regex in ['.*GEX', '.*VDJ']:
+        r = re.compile(regex)
+        file_subset = list(filter(r.match, files))
+        pools = [pd.read_csv(os.path.join(Folder, file)) for file in file_subset]
+        VDJ_GEX_list.append(pools)
+        # print(file_subset)
+    return VDJ_GEX_list
+
+#this is to merge the matched VDJ and GEX reads into one dataframe
+def merge_VDJ_and_GEX(GEX, VDJ):
+    VDJ = VDJ.drop(columns=['ReadIDs'])
+    GEX = GEX.drop(columns=['ReadIDs'])
+    merged = pd.merge(GEX, VDJ, on="Cellbarcode", how='outer')
+    merged.fillna(0, inplace=True)
+    return merged
+
+#this is to add a column containing information as to whether a cell is a carT cell to the adata object
+def annotate_mapped_cars(andata_obj, list_of_annotated_pools):
+    if "dataset" in andata_obj.obs:
+        intersection_all = []
+        for dataset in np.unique(andata_obj.obs.dataset):
+            subset = andata_obj.obs[andata_obj.obs['dataset'] == dataset] #note: dataset is a string for some reason
+            # print(subset.index)
+            subset_barcodes = [s[:16] for s in subset.index]
+            # print(subset_barcodes)
+            matches = set(list_of_annotated_pools[int(dataset)].Cellbarcode)
+            # print(matches)
+            intersection_subset = [barcode in matches for barcode in subset_barcodes]
+            # print(sum(intersection_subset))
+            intersection_all.extend(intersection_subset)
+    else:
+        print('Column "dataset" non existent in andata object')
+    andata_obj_cpy = andata_obj.copy()
+    andata_obj_cpy.obs['isCAR'] = intersection_all
+    return andata_obj_cpy
