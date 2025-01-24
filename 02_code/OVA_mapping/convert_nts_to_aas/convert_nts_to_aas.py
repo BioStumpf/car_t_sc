efbcdb4b3ae1d9b4d4dd03b06b98e9fa9c08f464
BioStumpf <david-dsf@gmx.de>
2025-01-24 18:12:59 +0100

    try mutliprocessing for speed

--- 02_code/OVA_mapping/convert_nts_to_aas/convert_nts_to_aas.py
+++ 02_code/OVA_mapping/convert_nts_to_aas/convert_nts_to_aas.py
@@ -7,6 +7,7 @@ from Bio.SeqRecord import SeqRecord
 from Bio.Seq import MutableSeq
 import gzip
 import argparse
+from multiprocessing import Pool
 
 #helper function for avoiding errrors due to nucleotide sequences not being a multiple of 3
 def multiple3(self): 
@@ -16,19 +17,37 @@ def multiple3(self):
     return self
 MutableSeq.multiple3 = multiple3
 
+def process_record(record):
+    seq = MutableSeq(record.seq)
+    results = []
+    for start in range(0,3):
+        translated = seq[start:].multiple3().translate().replace('*', 'X')
+        results.append(SeqRecord(translated, id=record.id, description=f"frame{start+1}"))
+    return results
+
 #main funtion capable of converting a nucleotide fastq.gz file into a fasta output, note each read_id is transferred 3 times, once for each open reading frame
 #also, nucleotide triplets not encoding for any aminoacid at all are transferred into an arbitrary nucleotide, denoted by 'X'
+# def translate_fastq_to_fasta_out(args):
+#     output = args.output
+#     input = args.input
+#     with gzip.open(output, 'wt') as output_handle: 
+#         with gzip.open(input, "rt") as input_file:
+#             for record in SeqIO.parse(input_file, 'fastq'):
+#                 seq = MutableSeq(record.seq)
+#                 for start in range(0,3):
+#                     translated = seq[start:].multiple3().translate().replace('*', 'X')
+#                     new_record = SeqRecord(translated, id = record.id, description=f"frame{start+1}")
+#                     SeqIO.write(new_record, output_handle, "fasta")
+
 def translate_fastq_to_fasta_out(args):
     output = args.output
     input = args.input
-    with gzip.open(output, 'wt') as output_handle: 
-        with gzip.open(input, "rt") as input_file:
-            for record in SeqIO.parse(input_file, 'fastq'):
-                seq = MutableSeq(record.seq)
-                for start in range(0,3):
-                    translated = seq[start:].multiple3().translate().replace('*', 'X')
-                    new_record = SeqRecord(translated, id = record.id, description=f"frame{start+1}")
-                    SeqIO.write(new_record, output_handle, "fasta")
+    with gzip.open(output, 'wt') as output_handle:
+        input_iterator = SeqIO.parse(gzip.open(input, "rt"), 'fastq')
+        
+        with Pool() as pool:
+            for record_set in pool.imap(process_record, input_iterator):
+                SeqIO.write(record_set, output_handle, "fasta")
 
 def main():
     parser=argparse.ArgumentParser(description="Convert the fq.gz illumina sequencing fastqs which are nucleotide sequences into protein sequences, base on all 3 ORFs")
