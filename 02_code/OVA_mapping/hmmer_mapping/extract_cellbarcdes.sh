88364e5981461e679421ac3bb054f8a053fd1c08
BioStumpf <david-dsf@gmx.de>
2025-02-06 15:03:25 +0100

    correcting for R1 instead R2

--- 02_code/OVA_mapping/hmmer_mapping/extract_cellbarcdes.sh
+++ 02_code/OVA_mapping/hmmer_mapping/extract_cellbarcdes.sh
@@ -2,12 +2,12 @@
 
 #specify Data and output directories
 DATA_DIR="$HOME/car_t_sc/01_data"
-FASTQS_DIR="$DATA_DIR/processed/OVA_receptor/hmmersearch_alignment_outputs/fastq_subsets"
+FASTQS_DIR="$DATA_DIR/processed/OVA_receptor/hmmersearch_alignment_outputs/fastq_subsets/R1"
 OUTPUT="$DATA_DIR/processed/OVA_receptor/OVA_counts"
 SCRIPT="$HOME/car_t_sc/02_code/OVA_mapping/hmmer_mapping/extract_cellbarcodes.py"
 ALIGNMENT_DIR="$DATA_DIR/processed/OVA_receptor/hmmersearch_alignment_outputs/non_tcr_filtered_tabular_outs"
 
-for file in $FASTQS_DIR/*{GEX,VDJ}*.fq.gz; do #{GEX,VDJ} for both VDJ and GEX
+for file in $FASTQS_DIR/*{GEX,VDJ}*R1.fq.gz; do #{GEX,VDJ} for both VDJ and GEX
     pool_pattern=$(basename $file | grep -o P[1-9])
     lib_pattern=$(basename $file | grep -oE "GEX|VDJ") #GEX|VDJ
     output_file="$OUTPUT/${pool_pattern}_${lib_pattern}.csv"
